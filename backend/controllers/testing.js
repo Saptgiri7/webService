d71a59df9aab432e8fa3b5970326c88d9f210b27
Saptgiri <124383493+Saptgiri7@users.noreply.github.com>
2025-11-10 17:14:23 +0530

    Refactor testServer to use ES6 export syntax

--- backend/controllers/testing.js
+++ backend/controllers/testing.js
@@ -1,3 +1,8 @@
-export const testServer = (req,res) =>{
+const testServer = (req,res) =>{
   res.status(200).json({message : `working with no issues`});
 }
+
+
+module.exports = {
+  testServer,
+};
